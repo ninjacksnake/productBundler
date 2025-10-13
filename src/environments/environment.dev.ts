31f3b5168b3be3404a3e05e63fefeeee4ef83ad9
Michael Fermin <ninjacksnake@gmail.com>
2025-10-13 15:16:59 -0400

    Adding environment variables

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api/v1',
+    refresshUrl: 'http://localhost:3000/api/v1/auth/refresh',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://10.0.0.50/api/v1',
+    refresshUrl: 'http://10.0.0.50/api/v1/auth/refresh',
+};
